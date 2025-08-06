4908d008a6b9cdde61a6feeece337e1909dcc942
home1344 <saliankhamkhamtanh@gmail.com>
2025-08-06 09:19:58 +0700

    Updated package.json

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Toaster } from 'react-hot-toast';
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <>
+      <App />
+      <Toaster 
+        position="top-right"
+        toastOptions={{
+          duration: 4000,
+          style: {
+            background: '#363636',
+            color: '#fff',
+          },
+        }}
+      />
+    </>
   </React.StrictMode>
 );
