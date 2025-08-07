c2e5c3a2a7948c4b5a5e4f6d680b64fe3202a218
Rasta <laotrader23@gmail.com>
2025-08-07 04:27:02 +0300

    Add wallet connection and LINK balance monitoring

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['@base-org/account']
-  },
   server: {
     port: 3001
   }
