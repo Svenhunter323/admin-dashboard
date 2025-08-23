e8d165be3fb829db2f73429ba375bc270b6b12bb
Rasta <laotrader23@gmail.com>
2025-08-23 07:18:29 +0700

    beta1

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: import.meta.env.VITE_API_BASE_URL,
-  baseURL: "http://localhost:5000", // Replace with your actual API base URL
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  // baseURL: "http://localhost:5000", // Replace with your actual API base URL
 });
 
 // Request interceptor to add auth token
---
+++ src/dist/assets/en_US-RFN65H63-c1918b76.js
@@ -0,0 +1,1 @@
+import{e as f}from"./index-f21bac5f.js";export{f as default};
